8787dc291279522c7008d8b4d0282922b7692551
Kit Lawes <kitlawes@hotmail.com>
2017-03-21 19:15:03 +0000

    Pass 14 tests for hamming exercise

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,8 @@
 class Hamming
   def self.compute(sequence_1, sequence_2)
+    if sequence_1.length != sequence_2.length
+      raise ArgumentError
+    end
     hamming_distance = 0
     for i in 0..(sequence_1.length - 1)
       if sequence_1[i] != sequence_2[i]
