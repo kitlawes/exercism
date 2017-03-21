470a43d03388342efb0e217fac79cce9b53029cf
Kit Lawes <kitlawes@hotmail.com>
2017-03-21 19:13:04 +0000

    Pass first seven tests for hamming exercise

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,7 +1,7 @@
 class Hamming
   def self.compute(sequence_1, sequence_2)
     hamming_distance = 0
-    sequence_1.split("").each do |i|
+    for i in 0..(sequence_1.length - 1)
       if sequence_1[i] != sequence_2[i]
         hamming_distance += 1
       end
