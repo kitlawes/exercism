26db801fef525b48c9a6f7b0404ada2fc75bca27
Kit Lawes <kitlawes@hotmail.com>
2017-03-23 13:37:23 +0000

    Complete raindrops exercise in Ruby

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,12 +1,22 @@
 class Raindrops
   def self.convert(number)
-    case number
-    when 1
-      return "1"
-    when 3
-      return "Pling"
-    when 5
-      return "Plang"
+    output = ""
+    if number % 3 == 0
+      output += "Pling"
     end
+    if number % 5 == 0
+      output += "Plang"
+    end
+    if number % 7 == 0
+      output += "Plong"
+    end
+    if output == ""
+      output += number.to_s
+    end
+    return output
   end
+end
+
+module BookKeeping
+  VERSION = 3
 end
