5fdc78a4425e4e101f3bb5712378da1dcec66c43
Kit Lawes <kitlawes@hotmail.com>
2017-03-23 13:31:46 +0000

    Change indentation in rna_transcription

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,22 +1,22 @@
 class Complement
-    def self.of_dna(dna)
-        complement = ""
-        for i in 0..(dna.length - 1)
-            case dna[i]
-            when "G"
-                complement += "C"
-            when "C"
-                complement += "G"
-            when "T"
-                complement += "A"
-            when "A"
-                complement += "U"
-            else
-                return ""
-            end
-        end
-        return complement
+  def self.of_dna(dna)
+    complement = ""
+    for i in 0..(dna.length - 1)
+      case dna[i]
+      when "G"
+        complement += "C"
+      when "C"
+        complement += "G"
+      when "T"
+        complement += "A"
+      when "A"
+        complement += "U"
+      else
+        return ""
+      end
     end
+    return complement
+  end
 end
 
 module BookKeeping
