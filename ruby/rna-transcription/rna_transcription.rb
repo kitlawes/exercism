71b45443c7fdffb18f421cc5b0f3929ec2575ef0
Kit Lawes <kitlawes@hotmail.com>
2017-03-23 13:23:39 +0000

    Pass 5 tests in rna_transcription_test

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,14 +1,18 @@
 class Complement
-  def self.of_dna(dna)
-    case dna
-        when "G"
-            return "C"
-        when "C"
-            return "G"
-        when "T"
-            return "A"
-        when "A"
-            return "U"
+    def self.of_dna(dna)
+        complement = ""
+        for i in 0..(dna.length - 1)
+            case dna[i]
+            when "G"
+                complement += "C"
+            when "C"
+                complement += "G"
+            when "T"
+                complement += "A"
+            when "A"
+                complement += "U"
+            end
+        end
+        return complement
     end
-  end
 end
